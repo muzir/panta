e50a56a37a53b6b1bbf858ba9ed65b6321fe9f07
muzir <>
2022-01-30 22:12:24 +0200

    Replace spectron with playwright

--- __tests__/AppTest.js
+++ __tests__/AppTest.js
@@ -1,49 +1,35 @@
-const Application = require("spectron").Application;
-const electron = require("electron");
+const { _electron: electron } = require('playwright');
 const path = require("path");
 const clipboardy = require('clipboardy')
 
 let app;
 
-jest.setTimeout(60000)
+jest.setTimeout(10000)
 process.env.PROFILE = 'integration'
-
-beforeEach(() => {
-  app = new Application({
-    path: electron,
-
-    args: [path.join(__dirname, "../")]
-  });
-
-  return app.start();
-}, 15000);
-
-afterEach(function () {
-  if (app && app.isRunning()) {
-    return app.stop();
-  }
-});
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-test("Displays App window", async function () {
-  let windowCount = await app.client.getWindowCount();
+beforeEach(async () => {
+    app =  await electron.launch({ args: ['main.js'] });
+}, 15000);
 
-  expect(windowCount).toBe(1);
+
+
+test("Displays App window", async function () {
+  let windowCount = await app.windows().length;
+  expect(windowCount === 1).toBeTruthy()
 });
 
 test("first element listed in items after write to clipboard", async function () {
   clipboardy.writeSync('💖 pasta!')
   await sleep(200)
-  const firstElement = await app.client.$("//*[@id=\"1\"]");
-  let firstElementText = await firstElement.getText();
-  expect(firstElementText).toBe('💖 pasta!');
+  const window = await app.firstWindow();
+  const value = await window.locator('//*[@id=\"1\"]').inputValue();
+  expect(value === '💖 pasta!').toBeTruthy();
 });
 
-test("Displays App title with app name and version", async function () {
-  app.browserWindow.getTitle().then((title)=>{
-    expect(title).toBe('panta v0.1.3');  
-  })
+afterEach(async () => {
+  await app.close();
 });
