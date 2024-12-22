c9c59f3e60ba52324cf7e84e8eb070cad140e82f
muzir <>
2024-12-22 17:58:54 +0200

    Set timeout as 90 second

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,9 +30,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
-    testIdAttribute: 'data-pw',
   },
+
+  // Each test is given 30 seconds.
+  timeout: 90000,
 
   /* Configure projects for major browsers */
   projects: [
